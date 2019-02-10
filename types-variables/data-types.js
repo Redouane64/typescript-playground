1946559fc922da01fffc778ab4ee9b7812627942
Redouane Sobaihi <s-redouane@outlook.com>
2019-02-11 01:51:57 +0700

    data types

---
+++ types-variables/data-types.js
@@ -0,0 +1,20 @@
+var foo = 42;
+var bar = "Hello World!";
+var names = ["Karim", "Ali", "Rachid", "Farid"];
+console.info(typeof foo + ": " + foo, typeof bar + ": " + bar);
+console.info(typeof names + ": ");
+names.forEach(function (element) {
+    console.log(element);
+});
+// enums
+var types;
+(function (types) {
+    types[types["Number"] = 2] = "Number";
+    types[types["Text"] = 4] = "Text";
+    types[types["Image"] = 8] = "Image";
+    types[types["Stream"] = 15] = "Stream";
+})(types || (types = {}));
+;
+var test = types.Image;
+console.info(typeof test, typeof types[test]);
+console.info(test, types[test]);
---
+++ types-variables/data-types.ts
@@ -0,0 +1,19 @@
+let foo: number = 42;
+let bar: string = "Hello World!";
+
+let names: string[] = ["Karim", "Ali", "Rachid", "Farid"];
+
+console.info(`${typeof foo}: ${foo}`, `${typeof bar}: ${bar}`);
+
+console.info(`${typeof names}: `);
+names.forEach(element => {
+    console.log(element);
+});
+
+// enums
+enum types { Number = 0x2, Text = 0x4, Image = 0x8, Stream = 0xf };
+
+let test: types = types.Image;
+
+console.info(typeof test, typeof types[test]);
+console.info(test, types[test]);
